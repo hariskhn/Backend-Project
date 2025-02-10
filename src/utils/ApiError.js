b0853e526a12d8d5f49c8e34a40ff9c8e57aae78
hariskhn <harislif3@gmail.com>
2025-02-11 01:46:51 +0500

    add register controller and bug fixes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,4 +1,4 @@
-class ApiError extends Error {
+class ApiError extends Error { // The purpose of this class is to standardize the error response that will be sent to the client.
     constructor(
         statusCode,
         message = "Something went wrong",
